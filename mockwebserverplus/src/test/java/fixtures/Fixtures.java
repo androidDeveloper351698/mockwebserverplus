ff99a317c1563ba9b986df0d78da71164c653c7b
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2016-10-24 03:49:06 +0200

    dependencies version bump code cleaning

--- mockwebserverplus/src/test/java/fixtures/Fixtures.java
+++ mockwebserverplus/src/test/java/fixtures/Fixtures.java
@@ -6,11 +6,11 @@ public class Fixtures {
     //no instance
   }
 
-  public static final String DELAYED_1000_MS = "delayed_1000_ms";
-  public static final String SIMPLE = "simple";
   public static final String SIMPLE_BODY_FILE = "simple_body_file";
-  public static final String SIMPLE_NO_BODY = "simple_no_body";
-  public static final String SIMPLE_NO_HEADERS = "simple_no_headers";
-  public static final String SIMPLE_NO_STATUS_CODE = "simple_no_status_code";
   public static final String SIMPLE_WITH_DELAY = "simple_with_delay";
+  public static final String SIMPLE_NO_STATUS_CODE = "simple_no_status_code";
+  public static final String SIMPLE_NO_BODY = "simple_no_body";
+  public static final String SIMPLE = "simple";
+  public static final String SIMPLE_NO_HEADERS = "simple_no_headers";
+  public static final String DELAYED_1000_MS = "delayed_1000_ms";
 }
